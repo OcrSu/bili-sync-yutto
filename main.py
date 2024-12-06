4502919506a8c705d27f2691f1764d5fb684ac4f
Suyiyi <75876201+OcrSu@users.noreply.github.com>
2024-12-06 09:18:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def download_video(media_id,bvid,video_dir):
         "1",
         "--no-color",
         "--no-progress",
-        "> log"
+        "--debug"
     ]
     try:
         subprocess_run(command, check=True)
